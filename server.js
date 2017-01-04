dd2d2fc6d4a109a2bc3089b939cc7fe5029e3ce5
Benjamin Neal <bengneal3@gmail.com>
2017-01-03 20:38:53 -0800

    Fixed favicon (1)

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var PORT = process.env.PORT || 9000;
 var favicon = require('serve-favicon');
 
 app.use(express.static(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/Login.html');
 })
+
+app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 
 app.listen(PORT, function () {
 	console.log('Express server started on port ' + PORT);
